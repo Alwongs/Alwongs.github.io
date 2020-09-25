b937162cc1c43688aa4fa309113c84c66855d221
Alex <66690469+Alwongs@users.noreply.github.com>
2020-09-25 19:54:52 +0400

    new site

---
+++ cv/js/data.js
@@ -0,0 +1,32 @@
+const data = {
+    "first_name": "Alex",
+    "last_name": "Wong",
+    "job_title": "DRIVER",
+    "photo": "images/me.jpg",
+    "phone": "+70370000037",
+    "email": "alwong@alwong.ru",
+    "skype": "alwong",
+    "linkedin": "Alwong_linkedin",
+    "address": "001A Kremlin, Moscow",
+    "skills": [
+        "Personal Driver",
+        "Driver of a bus",
+        "Driver of a truck",
+        "Builder of airplanes",
+        "Base of Javascript",
+        "Base of HTML",
+        "Base of CSS",
+        "PC user",
+        "Photoshop",
+        "Word",
+        "Excel",
+        "intermediate level of English",
+
+    ],
+    "education": [
+        ["2003-2009", "Samara AirSpace University"]
+    ],
+    "achievements": ["No achivements yet, but everything is ahead of me"],
+
+    "profile": "Hello! I am a good studying student going to be employed by you. Please don't hasitate to recruite me as soon as possible. While you were reading this paragraph I have already become a master, so don't be late or your competitors will be more sucsessful! "
+};
---
+++ cv/js/experience.js
@@ -0,0 +1,26 @@
+const experience = [
+    {
+        "company": "AVIASTAR",
+        "city": "Kirov",
+        "from": "2001",
+        "to": '2008',
+        "position": "Airbuilder",
+        "description": " Worked as an operator of software control  maschines. Started maschines using software written by programmers. ",
+    },
+    {
+        "company": "HOSPITAL",
+        "city": "Samara",
+        "from": "2008",
+        "to": '2010',
+        "position": "Driver",
+        "description": "Worked as a driver for hospital delivering medical personnel from hospital to people who need help"
+    },
+    {
+        "company": "Airlines",
+        "city": "Berezovka",
+        "from": "2010",
+        "to": 'Present',
+        "position": "Driver",
+        "description": "Work as a driver delivering personnel of the company, documents and cargos around the city"
+    },
+]
